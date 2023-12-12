c36046e34058de76ad67186d9420e5d04a1612ff
CuongDV45 <cuongdang10001@gmail.com>
2023-12-12 11:30:00 +0700

    In-Progress Course

--- src/app/pages/course/course.model.ts
+++ src/app/pages/course/course.model.ts
@@ -1,4 +1,5 @@
 export interface ICourse{
+category: any;
     id?: number;
     title?: string;
     price?: number;
@@ -6,7 +7,7 @@ export interface ICourse{
     end_time?: Date;
     enrollment_count?: number;
     thumbnail?: string;
-    is_delete?: boolean;
+    isActive?: boolean;
     description?: string;
     user_id?: number;
     category_id?: number;
@@ -23,15 +24,16 @@ export class Course implements ICourse{
         public end_time?: Date,
         public enrollment_count?: number,
         public thumbnail?: string,
-        public is_delete?: boolean,
+        public isActive?: boolean,
         public description?: string,
         public user_id?: number,
         public category_id?: number,
         public category_name?: string,
         public user_name?: string,
         ){
-        this.is_delete = is_delete ?? true;
+        this.isActive = isActive ?? true;
     }
+    category: any;
 }
 
 export function getCourseIdentifier(course: ICourse){
--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { ICourse } from '../pages/course/course.model';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { CategoryService } from './category.service';
+import { ICategory } from '../pages/category/category.model';
 
 export type EntityResponseType = HttpResponse<ICourse>;
 export type EntityArrayResponseType = HttpResponse<ICourse[]>;
@@ -12,18 +14,40 @@ export type EntityArrayResponseType = HttpResponse<ICourse[]>;
 export class CourseService {
   protected resourceUrl = `${environment.apiBaseUrl}/courses`;
   protected token = localStorage.getItem("access_token");
-  constructor(protected http: HttpClient) { }
+  constructor(
+    protected http: HttpClient,
+    private categoryService: CategoryService
+    ) { }
 
-  findAll(req?: any): Observable<EntityArrayResponseType> {
-    const headers = new HttpHeaders().set(
-      "Authorization",
-      `Bearer ${this.token}`
-    );
+  // findAll(req?: any): Observable<EntityArrayResponseType> {
+  //   const headers = new HttpHeaders().set(
+  //     "Authorization",
+  //     `Bearer ${this.token}`
+  //   );
+  //   const options = {
+  //     headers: headers,
+  //     observe: "response" as "response",
+  //   };
+  //   return this.http.get<ICourse[]>(`${this.resourceUrl}`, options);
+  // }
+
+  create(discount: ICourse): Observable<EntityResponseType>{
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${this.token}`)
     const options = {
       headers: headers,
-      observe: "response" as "response",
-    };
-    return this.http.get<ICourse[]>(`${this.resourceUrl}`, options);
+      observe: 'response' as 'response'
+    }
+    return this.http.post<ICourse>(this.resourceUrl, discount, options); 
+  }
+
+  findAll(): Observable<EntityArrayResponseType> {
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${this.token}`)
+    
+    const options = {
+      headers: headers,
+      observe: 'response' as 'response'
+    }
+    return this.http.get<ICourse[]>(this.resourceUrl, options);
   }
 
 }
