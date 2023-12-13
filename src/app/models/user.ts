599eda216fc5ee08a2fd9018231138c78e70302c
cuonglazy <cuong03705@gmail.com>
2023-12-13 16:14:43 +0700

    in-progress: user-profile

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,18 +1,40 @@
 import { Role } from "./role";
 import { UserImage } from "./user.image";
 export interface User {
-    id: number;
-    fullName: string;
-    phoneNumber: string;
-    email: string;
-    address: string;
-    password: string;
-    active: boolean;
-    dateOfBirth: Date;
-    facebook_account_id: number;
-    google_account_id: number;
-    image_url: string;
-    url: string;
-    role_id: Role;
-    user_image: UserImage;
+    id?: number;
+    fullName?: string;
+    phoneNumber?: string;
+    email?: string;
+    address?: string;
+    password?: string;
+    active?: boolean;
+    date_of_birth?: Date;
+    facebook_account_id?: number;
+    google_account_id?: number;
+    image_url?: string;
+    url?: string;
+    role_id?: Role;
+    user_image?: UserImage;
+}
+
+export class IUser implements User {
+    constructor (public id?: number,
+        public fullName?: string,
+        public phoneNumber?: string,
+        public email?: string,
+        public address?: string,
+        public password?: string,
+        public active?: boolean,
+        public date_of_birth?: Date,
+        public facebook_account_id?: number,
+        public google_account_id?: number,
+        public image_url?: string,
+        public url?: string,
+        public role_id?: Role,
+        public user_image?: UserImage
+        ) {
+        this.active = this.active ?? true;
+
+        }
+        
 }
--- src/app/pages/user-profile/user-profile.module.ts
+++ src/app/pages/user-profile/user-profile.module.ts
@@ -6,9 +6,10 @@ import { FormsModule } from "@angular/forms";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { UserProfileComponent } from "./list/user-profile.component";
 import { UpdateUserProfileComponent } from "./update/update.user-profile.component";
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
   imports: [UserProfileRoutingModule, ClipboardModule, FormsModule, NgbModule, CommonModule],
-  declarations: [UserProfileComponent, UpdateUserProfileComponent],
+  declarations: [UserProfileComponent, UpdateUserProfileComponent, UserEditComponent],
 })
 export class UserProfileModule {}
