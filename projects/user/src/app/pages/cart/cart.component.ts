1a827b65e6d967c9bd762853aee4474623457c94
cuonglazy <cuong03705@gmail.com>
2023-12-16 22:33:42 +0700

    in-progress: CartItem

--- projects/user/src/app/pages/cart/cart.component.ts
+++ projects/user/src/app/pages/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
@@ -6,36 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
-  carts : any = [
-    {
-      "name":"Khóa Học làm Giàu 1",
-      "price": "999 VNĐ",
-      "img":"../../assets/images/thumb-1.png"
-    },
-    {
-      "name":"Khóa Học làm Giàu 2",
-      "price": "999 VNĐ",
-      "img":"../../assets/images/thumb-2.png"
-    },
-    {
-      "name":"Khóa Học làm Giàu 3",
-      "price": "999 VNĐ",
-      "img":"../../assets/images/thumb-3.png"
-    },
-    {
-      "name":"Khóa Học Tiêu Tiền 1",
-      "price": "999 VNĐ",
-      "img":"../../assets/images/thumb-4.png"
-    },
-    {
-      "name":"Khóa Học Tiêu Tiền 2",
-      "price": "999 VNĐ",
-      "img":"../../assets/images/thumb-5.png"
-    },
-    {
-      "name":"Khóa Học Tiêu Tiền 3",
-      "price": "999 VNĐ",
-      "img":"../../assets/images/thumb-6.png"
-    },
-  ]
+  cartItemList: any = [];
+  courseList = new BehaviorSubject<any>([]);
+  constructor(){}
 }
--- projects/user/src/app/pages/playlist/playlist.component.ts
+++ projects/user/src/app/pages/playlist/playlist.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { IUser, User } from 'src/app/models/user';
 import { ICourse } from 'src/app/pages/course/course.model';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-playlist',
@@ -9,8 +11,9 @@ import { ICourse } from 'src/app/pages/course/course.model';
   styleUrls: ['./playlist.component.css']
 })
 export class PlaylistComponent implements OnInit{
+  user: any;
   dataCourse: any;
-  constructor(private activatedRoute: ActivatedRoute){
+  constructor(private activatedRoute: ActivatedRoute, protected userService: UserService){
   }
 
   ngOnInit(): void {
@@ -24,6 +27,10 @@ export class PlaylistComponent implements OnInit{
     this.dataCourse.start_time = this.dataCourse.start_time.replace('T', ' ');
     this.dataCourse.end_time = this.dataCourse.end_time.replace('T', ' ');
     console.warn(this.dataCourse);
-    
+    this.userService.getUserById(this.dataCourse.user_id).subscribe((res)=>{
+      this.user = res;
+      this.dataCourse.user_name = this.user.fullname;
+    })
   }
+
 }
