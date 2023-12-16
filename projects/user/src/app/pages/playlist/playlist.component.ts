54249c09c97ab6f232cfee4de91dd194b9161911
TruongNS5 <TruongNS5@fpt.com>
2023-12-16 19:07:50 +0700

    in progress

--- projects/user/src/app/pages/playlist/playlist.component.ts
+++ projects/user/src/app/pages/playlist/playlist.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ICourse } from 'src/app/pages/course/course.model';
+import { DiscountService } from 'src/app/service/discount.service';
 
 @Component({
   selector: 'app-playlist',
@@ -10,7 +11,9 @@ import { ICourse } from 'src/app/pages/course/course.model';
 })
 export class PlaylistComponent implements OnInit{
   dataCourse: any;
-  constructor(private activatedRoute: ActivatedRoute){
+  dataDiscount:any;
+  discountsWithCourseId: any[] = [];
+  constructor(private activatedRoute: ActivatedRoute, private discountService: DiscountService){
   }
 
   ngOnInit(): void {
@@ -24,6 +27,16 @@ export class PlaylistComponent implements OnInit{
     this.dataCourse.start_time = this.dataCourse.start_time.replace('T', ' ');
     this.dataCourse.end_time = this.dataCourse.end_time.replace('T', ' ');
     console.warn(this.dataCourse);
-    
+    this.discountService.findAll().subscribe(res =>{
+      this.dataDiscount = res.body ? res.body : [];
+      this.dataDiscount.forEach(discount => {
+        discount.discountCourses.forEach(discountCourse => {
+            if (discountCourse.course_id === this.dataCourse.user_id) {
+                this.discountsWithCourseId.push(discount);
+            }
+        });
+      });
+      console.warn("discount",this.discountsWithCourseId);
+    })
   }
 }
