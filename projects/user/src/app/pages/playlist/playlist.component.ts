900aa32d92eb3a8bb5c44f07aba63059d0773bbf
TruongNS5 <TruongNS5@fpt.com>
2023-12-17 12:12:12 +0700

    In-Progress : playlist & cart

--- projects/user/src/app/pages/playlist/playlist.component.ts
+++ projects/user/src/app/pages/playlist/playlist.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ICourse } from 'src/app/pages/course/course.model';
 import { DiscountService } from 'src/app/service/discount.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-playlist',
@@ -13,7 +14,10 @@ export class PlaylistComponent implements OnInit{
   dataCourse: any;
   dataDiscount:any;
   discountsWithCourseId: any[] = [];
-  constructor(private activatedRoute: ActivatedRoute, private discountService: DiscountService){
+  discountPercentage = 0;
+  key = "cart_item";
+  ItemCart : any;
+  constructor(private activatedRoute: ActivatedRoute, private discountService: DiscountService, private usersService: UserService){
   }
 
   ngOnInit(): void {
@@ -24,19 +28,96 @@ export class PlaylistComponent implements OnInit{
 
   support(course: any):void{
     this.dataCourse = course.course;
+    this.replaceTWithSpace();
+    this.fullNameUser();
+    this.priceDiscount();
+    console.warn(this.dataCourse);
+    this.disableAddCart();
+  }
+
+  // thay thế T bằng dấu cách
+  replaceTWithSpace():void{
     this.dataCourse.start_time = this.dataCourse.start_time.replace('T', ' ');
     this.dataCourse.end_time = this.dataCourse.end_time.replace('T', ' ');
-    console.warn(this.dataCourse);
-    this.discountService.findAll().subscribe(res =>{
-      this.dataDiscount = res.body ? res.body : [];
-      this.dataDiscount.forEach(discount => {
-        discount.discountCourses.forEach(discountCourse => {
-            if (discountCourse.course_id === this.dataCourse.user_id) {
-                this.discountsWithCourseId.push(discount);
-            }
-        });
-      });
-      console.warn("discount",this.discountsWithCourseId);
+  }
+
+  // Thêm Fullname của user vào dataCourse
+  fullNameUser():void{
+    this.usersService.getUserById(this.dataCourse.user_id).subscribe((res)=>{
+      const userName = res.fullname;
+      this.dataCourse.fullname = userName;
     })
+  }
+
+  // giá Khóa Học sao khi giảm giá 
+  priceDiscount():void{
+    this.discountService.findAll().subscribe(res => {
+      this.dataDiscount = res.body ? res.body : [];
+      for (const discount of this.dataDiscount) {
+        for (const course of discount.discountCourses) {
+          if (course.course_id === this.dataCourse.id) {
+            if(discount.percentage > this.discountPercentage){
+              this.discountPercentage = discount.percentage;
+            }
+            break;
+          }
+        }
+      }    
+      const priceDiscount = (this.dataCourse.price - ( this.dataCourse.price * this.discountPercentage / 100)).toFixed(2);
+      this.dataCourse.priceDiscount = priceDiscount;
+    });
+  }
+
+  // Hàm lưu giá trị vào localStorage
+  saveToLocalStorage(value: any): void {
+    try {
+      // Chuyển đối giá trị thành chuỗi trước khi lưu vào localStorage
+      const serializedValue = JSON.stringify(value);
+      localStorage.setItem(this.key, serializedValue);
+    } catch (error) {
+      console.error('Error saving to localStorage:', error);
+    }
+  }
+
+  // Hàm đọc giá trị từ localStorage
+  getFromLocalStorage(key: string): any | null {
+    try {
+      // Đọc giá trị từ localStorage và chuyển đổi thành đối tượng JavaScript
+      const serializedValue = localStorage.getItem(key);
+      const test = serializedValue ? JSON.parse(serializedValue) : null;
+      console.warn(test);      
+      return test;
+    } catch (error) {
+      console.error('Error getting from localStorage:', error);
+      return null;
+    }
+  }
+
+
+  addToLocalStorage(): void {
+    try {
+      // Đọc giá trị từ localStorage
+      const currentItems = this.getFromLocalStorage(this.key) || [];
+
+      // Thêm phần tử mới vào mảng hiện tại
+      currentItems.push(this.dataCourse);
+
+      // Lưu mảng mới vào localStorage
+      this.saveToLocalStorage(currentItems);
+    } catch (error) {
+      console.error('Error adding to localStorage:', error);
+    }
+  }
+
+  disableAddCart():void{
+    const serializedValue = localStorage.getItem(this.key);
+    const convertObject = JSON.parse(serializedValue);
+
+    const filteredItems = convertObject.filter((item: any) => item.id === this.dataCourse.id);
+    if(filteredItems.length > 0){
+      this.ItemCart = 1
+    }else{
+      this.ItemCart = 0
+    }
   }
 }
